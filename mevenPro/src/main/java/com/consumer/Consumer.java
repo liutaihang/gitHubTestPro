e6c0d2b6e3a0798e04257c62a00c96cd7272e1a6
liutaihang <1576756228qq.com>
2017-06-07 17:04:25 +0800

    Initial commit

--- mevenPro/src/main/java/com/consumer/Consumer.java
+++ mevenPro/src/main/java/com/consumer/Consumer.java
@@ -9,8 +9,10 @@ import com.rabbitmq.client.ConsumerCancelledException;
 import com.rabbitmq.client.QueueingConsumer;
 import com.rabbitmq.client.ShutdownSignalException;
 
+import po.Tree;
+
 public class Consumer {
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException, ClassNotFoundException {
 		Connection connection = MqTest.getClient();
 		Channel channel = connection.createChannel();
 		
@@ -21,21 +23,24 @@ public class Consumer {
 		channel.basicQos(10); 
 		boolean ack = false; // 是否自动确认消息被成功消费
 		channel.queueDeclare(MqTest.queueName, true, false, true, null);
+		//路由键exchange设置
+//		channel.exchangeDeclare("test_topic", "fanout");
+//		channel.queueDelete(MqTest.queueName);
+//		channel.queueBind(MqTest.queueName, "test_topic", MqTest.queueName);
+		
 		channel.basicConsume(MqTest.queueName, ack, consumer);
+		System.err.println("wait message ......");
 		while(true){
 			try {
 				QueueingConsumer.Delivery delivery = consumer.nextDelivery();
-				String message = new String(delivery.getBody());
+//				String message = new String(delivery.getBody());
+				Tree message = (Tree) MqTest.converToObj(delivery.getBody());
 				System.out.println(message);
-				channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
 			} catch (ShutdownSignalException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			} catch (ConsumerCancelledException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
