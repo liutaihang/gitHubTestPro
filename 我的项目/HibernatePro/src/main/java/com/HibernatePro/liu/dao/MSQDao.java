f847688ca146d79a8a8a14d4273c342274062601
liutaihang <1576756228qq.com>
2017-09-25 20:12:04 +0800

    add：添加

--- 我的项目/HibernatePro/src/main/java/com/HibernatePro/liu/dao/MSQDao.java
+++ 我的项目/HibernatePro/src/main/java/com/HibernatePro/liu/dao/MSQDao.java
@@ -1,21 +1,50 @@
 package com.HibernatePro.liu.dao;
 
+import java.io.Serializable;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+
 import javax.annotation.Resource;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
-public class DaoH <T>{
+public class MSQDao<T extends Serializable, S extends Serializable> {
 
 	@Resource
 	private SessionFactory factory;
-	
-	private Session getsession(){
+
+	private Class<T> classType;
+
+	@SuppressWarnings("unchecked")
+	protected MSQDao() {
+		Type superClass = this.getClass().getGenericSuperclass();
+		// 获取参数泛型的类
+		if (superClass instanceof ParameterizedType) {
+			this.classType = (Class<T>) ((ParameterizedType) superClass).getActualTypeArguments()[0];
+		}
+	}
+
+	public Session getsession() {
 		return factory.getCurrentSession();
 	}
-	
-	public T save(T t){
+
+	public T save(T t) {
 		getsession().save(t);
 		return t;
+	}
+
+	public T dele(T t) {
+		getsession().delete(t);
+		return t;
+	}
+
+	public T update(T t) {
+		getsession().update(t);
+		return t;
+	}
+
+	public T findByid(S s) {
+		return (T) getsession().get(classType, s);
 	}
 }
---
+++ 我的项目/HibernatePro/src/main/webapp/WEB-INF/jsp/index.jsp
@@ -0,0 +1,34 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>index</title>
+<script type="text/javascript" src="../js/jquery.min.js"></script>
+<link rel="stylesheet" href="../css/bootstrap.min.css">
+<script type="text/javascript">
+	
+</script>
+
+</head>
+<body>
+	<div class="page-header center-block">
+		头
+	</div>
+
+	<div class="container-fluid">
+		<div class="row-fluid">
+			<div class="span2">
+				边
+			</div>
+			<div class="span6">
+				content
+			</div>
+			<div class="span2">
+				边
+			</div>
+		</div>
+	</div>
+</body>
+</html>
