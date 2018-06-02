29ad1a1cbb9905ce3bfe34e95206c123f9fcdec7
liusir <1576756228@qq.com>
2018-06-02 19:12:33 +0800

    commit

--- 我的项目/jpa_bootstrap/src/main/java/cn/lth/base/BaseRepository.java
+++ 我的项目/jpa_bootstrap/src/main/java/cn/lth/base/BaseRepository.java
@@ -2,7 +2,9 @@ package cn.lth.base;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.repository.NoRepositoryBean;
 
+@NoRepositoryBean
 public interface BaseRepository<T> extends JpaRepository<T, Long>, JpaSpecificationExecutor<T> {
 
 }
