048a840bab41d0bbfacb0dff21ed484a5839c29f
liutaihang <1576756228@qq.com>
2018-08-22 17:52:48 +0800

    commit

---
+++ 我的项目/sir/src/main/java/liu/sir/SirApplication.java
@@ -0,0 +1,12 @@
+package liu.sir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SirApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SirApplication.class, args);
+    }
+}
